3323c0554b357a9c3af993c76863df82527692dc
Robert Blutner <robert@blutner.de>
2018-04-22 23:35:40 +0200

    Use Bootstrap 3 instead of 4

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Trading UI
 
 User interface for the trading application
+
+## Paths
+
+* /account/{accountId}/positions
+* /account/{accountId}/transactions
+* /scoring
--- src/boot/Application.tsx
+++ src/boot/Application.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react'
 
 import 'bootstrap/dist/css/bootstrap.css'
+
 import AccountPositionList from '../components/account/AccountPositionList'
 import AccountTransactionList from '../components/account/AccountTransactionList'
 import RegisterTransactionForm from '../components/account/RegisterTransactionForm'
@@ -11,11 +12,22 @@ export class Application extends React.Component {
         return (
             <div className="container">
                 <div className="header">
-                    <div className="logo"><img src="static/logo.png" /></div>
+                    <div className="logo"><img src="/static/logo.png" /></div>
                     <div className="text">Trading Command and Control Center</div>
+                    <div className="clearfix"></div>
                 </div>
 
-                <div className="clearfix"></div>
+                <nav className="navbar navbar-default">
+                    <ul className="nav navbar-nav">
+                        <li><a href="">Overview</a></li>
+                        <li><a href="">Strategy</a></li>
+                        <li><a href="">Account</a></li>
+                        <li><a href="">Scoring</a></li>
+                        <li><a href="">Trades</a></li>
+                        <li><a href="">Market data</a></li>
+                        <li><a href="">Tax report</a></li>
+                    </ul>
+                </nav>
 
                 <div className="body">
                     <AccountPositionList/>
